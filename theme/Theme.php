8c5cae1bcd07d93e1dc5cc9ec7ea961151cd6154
seungmanchoi <blueng.choi@gmail.com>
2017-04-11 18:00:37 +0900

    info.php 개선

--- theme/Theme.php
+++ theme/Theme.php
@@ -9,11 +9,4 @@ class Theme extends GenericTheme
 {
     protected static $path = 'shop_homepage/theme';
 
-    public function renderSetting(ConfigEntity $config = null)
-    {
-        XeFrontend::js(static::asset('js/setting.js'))->load();
-
-        return parent::renderSetting($config);
-    }
-
 }
--- theme/info.php
+++ theme/info.php
@@ -3,88 +3,109 @@ return [
     'view' => 'theme',
     'setting' => [
         //레이아웃 선택
-        'layout' => [
-            '_type' => 'select',
-            '_section' => '설정',
-            'label' => '레이아웃',
-            'options' => [
-                'main' => 'Main 페이지용',
-                'sub' => 'Sub 페이지용',
+        [
+            'section' => [
+                'class' => 'setting-section',
+                'title' => '설정'
+            ],
+            'fields' => [
+                'layout' => [
+                    '_type' => 'select',
+                    'label' => '레이아웃',
+                    'options' => [
+                        'main' => 'Main 페이지용',
+                        'sub' => 'Sub 페이지용',
+                    ],
+                    'description' => '레이아웃을 사용할 페이지를 지정하세요.'
+                ],
+                'gnb' => [
+                    '_type' => 'menu',
+                    'label' => '메인 메뉴 설정',
+                    'description' => '메뉴를 지정하세요.'
+                ],
+                'logoTitle' => [
+                    '_type' => 'text',
+                    'label' => '로고 타이틀',
+                    'placeholder' => '로고 제목을 입력하세요',
+                    'description' => '로고 제목을 입력하세요',
+                ],
+                //footer 설정
+                'useFooter' => [
+                    '_type' => 'select',
+                    'label' => '푸터 영역 사용 여부',
+                    'options' => [
+                        'y' => '사용',
+                        'n' => '사용 안함'
+                    ],
+                    'description' => '푸터 사용 여부를 선택하세요',
+                ],
+                'footerCopyright' => [
+                    '_type' => 'textarea',
+                    'label' => '카피라이트',
+                    'description' => 'HTML 사용이 가능합니다.',
+                ],
             ]
         ],
-
-        'gnb' => [
-            '_type' => 'menu',
-            '_section' => '설정',
-            'label' => '메인 메뉴 설정',
-        ],
-
-        'logoTitle' => [
-            '_type' => 'text',
-            '_section' => '설정',
-            'label' => '로고 타이틀',
-            'placeholder' => '로고 제목을 입력하세요',
-            'description' => '로고 제목을 입력하세요',
-        ],
-
-        //footer 설정
-        'useFooter' => [
-            '_type' => 'select',
-            '_section' => '설정',
-            'label' => '푸터 영역 사용 여부',
-            'options' => [
-                'y' => '사용',
-                'n' => '사용 안함'
-            ]
-        ],
-        'footerCopyright' => [
-            '_type' => 'textarea',
-            '_section' => '설정',
-            'label' => '카피라이트',
-        ],
-
         /* Main 페이지 */
-        //top slider
-        'useTopSlide' => [
-            '_type' => 'select',
-            '_section' => 'Main 페이지',
-            'label' => '상단 슬라이드 사용 유무',
-            'options' => [
-                'y' => '사용',
-                'n' => '사용 안함'
+        [
+            'section' => [
+                'class' => 'main-section',
+                'title' => 'Main 페이지'
+            ],
+            'fields' => [
+                'useTopSlide' => [
+                    '_type' => 'select',
+                    'label' => '상단 슬라이드 사용 유무',
+                    'options' => [
+                        'y' => '사용',
+                        'n' => '사용 안함'
+                    ],
+                    'description' => '캐러셀 사용여부를 선택하세요.',
+                ],
+                'slide1Image' => [
+                    '_type' => 'image',
+                    'label' => '상단 슬라이드1 이미지',
+                    'description' => '800x300',
+                ],
+                'slide2Image' => [
+                    '_type' => 'image',
+                    'label' => '상단 슬라이드2 이미지',
+                    'description' => '800x300',
+                ],
+                'slide3Image' => [
+                    '_type' => 'image',
+                    'label' => '상단 슬라이드3 이미지',
+                    'description' => '800x300',
+                ],
             ]
         ],
-        'slide1Image' => [
-            '_type' => 'image',
-            '_section' => 'Main 페이지',
-            'label' => '상단 슬라이드1 이미지 (800x300)',
-        ],
-        'slide2Image' => [
-            '_type' => 'image',
-            '_section' => 'Main 페이지',
-            'label' => '상단 슬라이드2 이미지 (800x300)',
-        ],
-        'slide3Image' => [
-            '_type' => 'image',
-            '_section' => 'Main 페이지',
-            'label' => '상단 슬라이드3 이미지 (800x300)',
-        ],
-
         /* Sub 페이지 */
-        'useSubMenu' => [
-            '_type' => 'select',
-            '_section' => 'Sub 페이지',
-            'label' => '서브 메뉴 영역 사용',
-            'options' => [
-                'y' => '사용',
-                'n' => '사용 안함'
+        [
+            'section' => [
+                'class' => 'sub-section',
+                'title' => 'Sub 페이지'
+            ],
+            'fields' => [
+                'useSubMenu' => [
+                    '_type' => 'select',
+                    'label' => '서브 메뉴 영역 사용',
+                    'options' => [
+                        'y' => '사용',
+                        'n' => '사용 안함'
+                    ],
+                    'description' => '서브 메뉴 사용 여부를 선택하세요.',
+                ],
+                'subMenu' => [
+                    '_type' => 'menu',
+                    'label' => '서브 페이지 메뉴 설정',
+                    'description' => '서브 메뉴를 선택하세요.',
+                ],
             ]
         ],
-
-        'subMenu' => [
-            '_type' => 'menu',
-            '_section' => 'Sub 페이지',
-            'label' => '서브 페이지 메뉴 설정',
+    ],
+    'setting.assets' => [
+        'js' => [
+            'js/setting.js'
         ],
     ],
     'editable' => [
